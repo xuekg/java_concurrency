5349d5eab6f1ced3d65f45003357ec7040bb4333
xuekg <xuekg@dcholdings.com>
2021-04-11 22:26:14 +0800

    hashmap

---
+++ com.xuekg/hashmap/HashMapTest.java
@@ -0,0 +1,14 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+  public static void main(String[] args) {
+    HashMap<String, String> hashMap = new HashMap<>();
+
+    hashMap.put("key", "value");
+
+  }
+
+}
