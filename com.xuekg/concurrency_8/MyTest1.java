fd8fcfca933302bdb6133d9a524cb0e3b22775df
xuekg <xuekg@dcholdings.com>
2021-07-31 13:40:44 +0800

    test

--- com.xuekg/concurrency_8/MyTest1.java
+++ com.xuekg/concurrency_8/MyTest1.java
@@ -18,7 +18,7 @@ import java.util.stream.IntStream;
       DiscardOldestPolicy：丢弃掉阻塞队列中存放时间最久的任务（队头元素），并且为当前所提交的任务留出一个队列中的空闲空间，以便将其放进到队列中
       CallerRunsPolicy：直接由提交任务的线程来运行这个提交的任务
 
-      在线程池中，最好将偏向锁的标记关闭。
+      在线程池中，最好将偏向锁的标记关闭 。
  */
 public class MyTest1 {
     public static void main(String[] args) {
